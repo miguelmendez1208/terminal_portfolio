c6c7776555c8a1e0f5ee640fdf62ca2c705ed72b
miguelmendez1208 <miguelmendez1208@gmail.com>
2025-04-30 16:55:40 -0500

    hotfix. Fixed text color on devices that override the default text color. Because we take advantage of CSS the styles will cascade properly, so the shebangs and the symbols keep their unique color, while any other text, that falls outside of those unique font color requirements, are defaulted to white rgb(255,255,255).

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,24 +14,28 @@ export default {
         shebang: "#4e9a06",
         symbol: "#3465a4",
         command: "rgb(255,255,255)",
+        text: "rgb(255,255,255)"
       },
       ubuntu: {
         bgcol: "#300a24",
         shebang: "rgb(22, 101, 52)",
         symbol: "rgb(29, 78, 216)",
         command: "rgb(255,255,255)",
+        text: "rgb(255,255,255)"
       },
       matrix: {
         bgcol: "#000",
         shebang: "rgb(13, 238, 22)",
         symbol: "rgb(13, 238, 22)",
         command: "rgb(255,255,255)",
+        text: "rgb(255,255,255)"
       },
       arch: {
         bgCol: "rgb(51,51,51)",
         shebang: "rgb(29, 78, 216, 1)",
         symbol: "rgb(13, 238, 22, 1)",
-        command: "rgb(255,255,255)",
+        command: "rgb(255,255,255)", 
+        text: "rgb(255,255,255)"
       },
     }),
   ],
