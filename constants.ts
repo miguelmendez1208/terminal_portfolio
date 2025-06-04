f6857203501a5000a69c7c3b8280e67f082c636f
miguelmendez1208 <miguelmendez1208@gmail.com>
2025-06-04 05:39:01 -0500

    removed themes.

--- constants.ts
+++ constants.ts
@@ -2,4 +2,4 @@ export const username = import.meta.env.VITE_USERNAME ?? "visitor";
 export const hostname = import.meta.env.VITE_HOSTNAME ?? "portfolio";
 export const path = import.meta.env.VITE_PATH ?? "~";
 export const symbol = import.meta.env.VITE_SYMBOL ?? "$";
-export const themes: string[] = ["popOS", "matrix", "arch", "ubuntu", "msdos", "halfLife", "monokai", "dracula"];
+export const themes: string[] = ["popOS", "matrix", "arch", "ubuntu", "halfLife", "dracula"];
